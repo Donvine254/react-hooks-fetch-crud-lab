beef7f6a368f1bbc156647a81a26b0fc76719c9c
donvine mugendi <donvinemugendi@gmail.com>
2023-06-01 01:47:11 +0300

    fetched questions

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,24 @@ import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
+const baseUrl="http://localhost:4000/questions"
+
 function App() {
   const [page, setPage] = useState("List");
   const [questions, setQuestions]= useState([]);
 
+
+  //fetch the questions using useEffect
+  useEffect(() => {
+    fetch(baseUrl).then(response=>response.json())
+    .then(data=>setQuestions(data))
+  }, [])
+  
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList questions={questions} />}
     </main>
   );
 }
